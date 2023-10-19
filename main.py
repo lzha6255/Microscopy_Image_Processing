67093741c6cc68f9bd35c1a4797226b87dd89c01
lzha6255 <lzha6255@uni.sydney.edu.au>
2023-10-19 23:46:44 +1100

    Added function that gets the scale length in pixels.

--- main.py
+++ main.py
@@ -50,12 +50,40 @@ def colored_threshold_overlay(img, overlay, thresh, colour):
     return img_out
 
 
+# Gets the scale length in pixels by walking diagonally from bottom left corner until scale is reached and then walking
+# left and right to determine the length of the scale. Assumes that the scale is dark on a white background.
+def get_scale_length(img, thresh):
+    coords = [0, len(img)-1]
+    while True:
+        # Take step diagonally in NE direction
+        coords[0] = coords[0] + 1
+        coords[1] = coords[1] - 1
+        # Check if there has been a significant colour drop
+        if img[coords[1]+1][coords[0]-1] - img[coords[1]][coords[0]] > thresh:
+            break
+    # Remember x coordinate
+    x = coords[0]
+    length = [0, 0]
+    # Walking left
+    while img[coords[1]][coords[0]-1] - img[coords[1]][coords[0]] < thresh:
+        coords[0] = coords[0] - 1
+        length[0] = length[0] + 1
+    coords[0] = x
+    # Walking right
+    while img[coords[1]][coords[0]+1] - img[coords[1]][coords[0]] < thresh:
+        coords[0] = coords[0] + 1
+        length[1] = length[1] + 1
+    print(str(length[0] + length[1] + 1) + " pixels")       # Add 1 to count the pixel that was originally intercepted
+
+
 if __name__ == '__main__':
     ebsd_scan = "images\\A.tif"
     sobel.sobel(ebsd_scan)
     ebsd_img = cv.imread(ebsd_scan, cv.IMREAD_COLOR)
+    ebsd_img_gray = cv.cvtColor(ebsd_img, cv.COLOR_BGR2GRAY)
     img_to_file.img_to_file(canny_array(ebsd_img, 3, 3, 3, 30), ebsd_scan, "matrix", "png")
     ebsd_edges = canny.canny_img(ebsd_img, 36)
     ebsd_edges = cv.cvtColor(ebsd_edges, cv.COLOR_BGR2GRAY)
     ebsd_overlay = colored_threshold_overlay(ebsd_img, ebsd_edges, 127, [255, 0, 0])
     img_to_file.img_to_file(ebsd_overlay, ebsd_scan, "overlay", "png")
+    get_scale_length(ebsd_img_gray, 10)
